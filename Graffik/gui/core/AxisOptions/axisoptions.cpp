446e4ed8755086707b20187de6669020a7ec287f
C. A. Church <thisdroneeatspeople@gmail.com>
2012-06-11 18:20:16 +0000

    * Re-factoring

---
+++ Graffik/gui/core/AxisOptions/axisoptions.cpp
@@ -0,0 +1,130 @@
+#include "axisoptions.h"
+
+#include <QDebug>
+
+AxisOptions::AxisOptions(QObject *parent) :
+    QObject(parent)
+{
+
+    // allow us to serialize OMaxisOptions structures
+    // and use OMaxisOptions structures in arguments to
+    // signals and slots
+
+     qRegisterMetaType<OMaxisOptions>("OMaxisOptions");
+     qRegisterMetaTypeStreamOperators<OMaxisOptions>("OMaxisOptions");
+
+     m_optMutex = new QMutex(QMutex::Recursive);
+
+}
+
+AxisOptions::~AxisOptions() {
+
+    OMaxisOptions* opt;
+    foreach(opt, m_optList) {
+        delete opt;
+    }
+
+    m_optList.clear();
+
+    delete m_optMutex;
+}
+
+/** Get Options for a Specific Axis
+
+  Retrieves option data for a specific axis
+
+  @return
+  Pointer to OMaxisOptions structure
+  */
+
+OMaxisOptions* AxisOptions::getOptions(unsigned short p_addr) {
+
+    m_optMutex->lock();
+
+    if( ! m_optList.contains(p_addr) ) {
+        m_optMutex->unlock();
+        return 0;
+    }
+
+    OMaxisOptions* ret = m_optList.value(p_addr);
+
+    m_optMutex->unlock();
+
+    return ret;
+}
+
+/** Set Options for a Specific Axis
+
+  Sets option data for a specific axis
+
+  @param p_addr
+  The address of the axis
+
+  @param p_opts
+  A pointer to an OMaxisOptions structure.  While you need to create it, you do
+  not need to manage the lifetime of the pointer, and should always use the
+  getOptions() method to retrieve it.  This class wil manage the life of the
+  created structure for you.
+
+  */
+
+void AxisOptions::setOptions(unsigned short p_addr, OMaxisOptions *p_opts) {
+    m_optMutex->lock();
+    m_optList[p_addr] = p_opts;
+    m_optMutex->unlock();
+    emit deviceOptionsChanged(p_opts, p_addr);
+}
+
+
+
+/** Set a Specific Axis as Timing Master
+
+  Since only one device may be a timing master on the bus, when changing one device
+  to be the timing master, we need to change all others to be slaves.  This
+  method does this for you automatically.  Note, however, this only affects the
+  stored configuration for the node devices, and does not transmit new data
+  to them.  You still need to do this separately from this class.
+
+  @param p_addr
+  Address of the device that shall be the timing master
+  */
+
+void AxisOptions::setMaster(unsigned short p_addr) {
+    // only one axis can be a timing master
+
+    qDebug() << "OAFO: setMaster called for" << p_addr;
+
+    m_optMutex->lock();
+
+    QList<unsigned short> addresses = m_optList.keys();
+    unsigned short addr;
+
+    foreach(addr, addresses) {
+        if(addr == p_addr) {
+            qDebug() << "OAFO: setMaster: Found options for" << p_addr;
+            m_optList[addr]->master = true;
+            emit deviceOptionsChanged(m_optList[addr], addr);
+        }
+        else if(m_optList[addr]->master == true ) {
+                // only update the previous master, if there is one
+            m_optList[addr]->master = false;
+            emit deviceOptionsChanged(m_optList[addr], addr);
+        }
+    }
+
+    m_optMutex->unlock();
+}
+
+void AxisOptions::deviceAdded(OMbusInfo *p_bus, OMdeviceInfo *p_dev) {
+    qDebug() << "OAFO: Added new device" << p_dev->device->address();
+        // only create new settings if they do not already exist
+
+    m_optMutex->lock();
+
+    if( ! m_optList.contains(p_dev->device->address()) )
+        setOptions(p_dev->device->address(), new OMaxisOptions);
+
+    m_optMutex->unlock();
+}
+
+
---
+++ Graffik/gui/core/AxisOptions/axisoptionsdialog.cpp
@@ -0,0 +1,151 @@
+#include <QDebug>
+
+#include "axisoptionsdialog.h"
+#include "ui_axisoptionsdialog.h"
+
+#include "core/ConfirmDialog/confirmdialog.h"
+
+AxisOptionsDialog::AxisOptionsDialog(AxisOptions* c_opts, unsigned short c_addr, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OMAxisFilmOptionsDialog)
+{
+    ui->setupUi(this);
+    m_opts = c_opts->getOptions(c_addr);
+    m_optObj = c_opts;
+    m_addr = c_addr;
+
+    _initInputs();
+
+        // do NOT use Qt::QueuedConnection here - you want a direct connection on the accept signal, otherwise
+        // this object may be gone by the time the signal is handled.
+
+    QObject::connect(ui->defaultCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(_defaultComboChange(int)));
+    QObject::connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(_accept()));
+
+}
+
+AxisOptionsDialog::~AxisOptionsDialog()
+{
+    delete ui;
+}
+
+
+void AxisOptionsDialog::_initInputs() {
+
+        // populate "defaults" combo box
+    ui->defaultCombo->addItem("VX1 Pan", AXIS_VX1_PAN);
+    ui->defaultCombo->addItem("VX1 Tilt", AXIS_VX1_TILT);
+    ui->defaultCombo->addItem("SX1 Slider", AXIS_SX1_SLIDE);
+    ui->defaultCombo->addItem("Custom", AXIS_CUSTOM);
+
+        // get current "default" type for this axis
+    int combShowSel = ui->defaultCombo->findData(m_opts->axisType);
+    ui->defaultCombo->setCurrentIndex(combShowSel);
+
+    ui->typeCombo->addItem("Rotary", AXIS_MOVE_ROT);
+    ui->typeCombo->addItem("Linear", AXIS_MOVE_LIN);
+
+    // get current "default" type for this axis
+    combShowSel = ui->typeCombo->findData(m_opts->axisMove);
+    ui->typeCombo->setCurrentIndex(combShowSel);
+
+    ui->ratioLine->setText(QString::number(m_opts->ratio));
+    ui->maxLine->setText(QString::number(m_opts->maxSteps));
+
+    ui->masterCheck->setChecked(m_opts->master);
+
+        // disable inputs if one of the pre-defined types
+    if( m_opts->axisType != AXIS_CUSTOM ) {
+        ui->maxLine->setDisabled(true);
+        ui->ratioLine->setDisabled(true);
+        ui->typeCombo->setDisabled(true);
+
+    }
+
+}
+
+    // when the selection is changed on the default combo box..
+
+void AxisOptionsDialog::_defaultComboChange(int p_idx) {
+
+    bool enable = false;
+    unsigned int sel = ui->defaultCombo->itemData(p_idx).toUInt();
+
+    switch(sel) {
+        case AXIS_CUSTOM:
+            enable = true;
+            break;
+        case AXIS_VX1_PAN:
+            // pass down...
+        case AXIS_VX1_TILT:
+            enable = false;
+            ui->ratioLine->setText(QString::number(OM_OPT_VX1RATIO));
+            ui->maxLine->setText(QString::number(OM_OPT_VX1MAXSTEPS));
+            ui->typeCombo->setCurrentIndex(ui->typeCombo->findData(AXIS_MOVE_ROT));
+            break;
+        case AXIS_SX1_SLIDE:
+            enable = false;
+            ui->ratioLine->setText(QString::number(OM_OPT_SX1RATIO));
+            ui->maxLine->setText(QString::number(OM_OPT_SX1MAXSTEPS));
+            ui->typeCombo->setCurrentIndex(ui->typeCombo->findData(AXIS_MOVE_LIN));
+            break;
+    }
+
+        // pre-defined types cannot be customized
+    if( enable == false ) {
+        ui->maxLine->setDisabled(true);
+        ui->ratioLine->setDisabled(true);
+        ui->typeCombo->setDisabled(true);
+    }
+    else {
+        ui->maxLine->setDisabled(false);
+        ui->ratioLine->setDisabled(false);
+        ui->typeCombo->setDisabled(false);
+    }
+
+}
+
+    // slot connected to accept signal from buttonbox
+    // save dialog values
+
+void AxisOptionsDialog::_accept() {
+
+    qDebug() << "AFODia: User Saved Values";
+
+    m_opts->axisType = ui->defaultCombo->itemData(ui->defaultCombo->currentIndex()).toUInt();
+    m_opts->axisMove = ui->typeCombo->itemData(ui->typeCombo->currentIndex()).toUInt();
+    m_opts->ratio = ui->ratioLine->text().toFloat();
+    m_opts->maxSteps = ui->maxLine->text().toUInt();
+
+    m_opts->jogLimit = m_opts->maxSteps;
+    m_opts->jogDamp = OM_OPT_JOGDAMP;
+
+    if( m_opts->master != ui->masterCheck->checkState() ) {
+            // changing the timing master flag has implications, force the user to confirm
+            // do nothing if they do not confirm
+        if( m_opts->master == false ) {
+            QString confTxt = "By setting this device as the timing master, you will automatically unset any existing master device. Do you want to do this?";
+            ConfirmDialog dia(confTxt, this);
+            int res = dia.exec();
+            if( res == QDialog::Accepted ) {
+                m_optObj->setMaster(m_addr);
+            }
+        }
+        else {
+            QString confTxt = "If you unset this device as the timing master, you must manually select and edit another device and configure it as timing master for synchronization to work. Do you want to do this?";
+            ConfirmDialog dia(confTxt, this);
+            int res = dia.exec();
+            if( res == QDialog::Accepted ) {
+                m_opts->master = false;
+            }
+
+        }
+    }
+
+        // this may seem redundant since we're working with a pointer to the actual options,
+        // but it forces the axis options object to do any necessary cleanup and signaling
+        // after we muck with the contents
+
+    m_optObj->setOptions(m_addr, m_opts);
+
+}
